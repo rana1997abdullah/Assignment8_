3341ed2f938fc95f1435a5ca2954b8c92412ee4f
rana1997abdullah <rana1997abumazen@gmail.com>
2024-10-30 18:24:14 +0200

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var random = new System.Random();
+        for(int i=0;i<20;i++){
+            int num = random.Next(1,21);
+            if(num==15) break;
+            if(num==5) continue;
+            Debug.Log(num);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
